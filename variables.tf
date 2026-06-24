c763c59bea9edd795a44726f0801fc3802846001
gopika-jastack <gopikashree2000@gmail.com>
2026-06-24 13:03:09 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "location" {
+  description = "Azure Region"
+  default     = "UK South"
+}
+
+variable "resource_group_name" {
+  description = "Resource Group Name"
+  default     = "rg-vm-demo"
+}
+
+variable "admin_username" {
+  description = "VM Admin Username"
+  default     = "azureadmin"
+}
+
+variable "admin_password" {
+  description = "VM Admin Password"
+  sensitive   = true
+}
